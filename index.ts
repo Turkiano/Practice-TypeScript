335c6c75c7633c7fe2731ef16658b508a85734dc
Turkiano <turki.saeed.ksa@gmail.com>
2024-11-27 10:41:01 +0300

    working on Bank app

--- index.ts
+++ index.ts
@@ -1,3 +1,28 @@
-let testing: string = "hello World";
+class Bank {
+  name: string;
+  branches: Branch[];
 
-console.log(testing);
+  constructor(name: string) {
+    this.name = name;
+    this.branches = [];
+  }
+
+  //Create a new branch method
+  addBranch(branch: Branch) {
+    this.branches.push(branch);
+  }
+  //Read a all branches method
+  getBranches() {
+    return JSON.stringify(this.branches, null, 2);
+  }
+}
+
+class Branch {
+  name: string;
+  customers: [];
+
+  constructor(name: string) {
+    this.name = name;
+    this.customers = [];
+  }
+}
