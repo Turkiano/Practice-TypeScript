9511e670d5067a0b0b7568ac279d1e43697420bb
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-06 21:05:57 +0300

    Intersection & Aliases & Union types

--- index.js
+++ index.js
@@ -1,118 +1,22 @@
 "use strict";
-class Bank {
-    constructor(name) {
-        this.name = name;
-        this.branches = [];
-    }
-    addBranch(branch) {
-        this.branches.push(branch);
-        console.log(`this [${branch.name}] has been added successfully!`);
-    }
-    getBranches() {
-        return JSON.stringify(this.branches, null, 2);
-    }
+//assigning the aliases type to an object
+const student = {
+    name: 'John',
+    age: 38,
+    cohort: "C#"
+};
+console.log(student);
+//-------------------------Union Type Example 01--------------
+//two data types assigned to one single variable
+function printMessage(message) {
+    return message;
 }
-class Branch {
-    constructor(name) {
-        this.name = name;
-        this.customer = [];
-    }
-    getName() {
-        return this.name;
-    }
-    addCustomer(NewCustomer) {
-        this.customer.push(NewCustomer);
-    }
-    getCustomer() {
-        return JSON.stringify(this.customer, null, 2);
-    }
-    toJSON() {
-        return {
-            name: this.name,
-            customers: this.customer, // Serialize customer details
-        };
-    }
+printMessage("Success");
+printMessage("Failed");
+printMessage("Pending");
+printMessage(200);
+printMessage(404);
+//c.declaring a function using the the above two types
+function printMessage02(mesg) {
+    return mesg;
 }
-class Customer {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-        this.transaction = [];
-    }
-    getName() {
-        return this.name;
-    }
-    getId() {
-        return this.id;
-    }
-    getTransaction() {
-        return this.transaction;
-    }
-    getBalance() {
-        let balance = 0;
-        this.transaction.forEach((e) => {
-            // go through each transaction in the array
-            balance += e.amount; // Add current transaction to the balance
-        });
-        return balance; // Return the total balance
-    }
-    addTransaction(transaction) {
-        if (transaction.amount < 0 &&
-            Math.abs(transaction.amount) > this.getBalance()) {
-            return "no suficient balance"; // Rejects the transaction if the amount exceeds the balance
-        }
-        this.transaction.push(transaction);
-        return " transaction added successfully"; // Adds the transaction if the amount is positive
-    }
-    toJSON() {
-        return {
-            name: this.name,
-            id: this.id,
-            balance: this.getBalance(), // Include dynamic balance
-            transaction: this.transaction,
-        };
-    }
-}
-class Transaction {
-    constructor(amount, date) {
-        this.amount = amount;
-        this.date = date;
-    }
-}
-const bank01 = new Bank("myBank");
-const branch01 = new Branch("Downtown Branch");
-const branch02 = new Branch("Abdullah Branch");
-// console.log("//----------------------LevelOne---------");
-bank01.addBranch(branch01);
-bank01.addBranch(branch02);
-let getAllBranches = bank01.getBranches();
-// console.log("LevelOne: " + getAllBranches);
-// console.log("//----------------------LevelTwo---------");
-const customer01 = new Customer("Turkiano", 0);
-const customer02 = new Customer("Khalid", 1);
-branch01.addCustomer(customer01);
-branch01.addCustomer(customer02);
-//transactions
-customer01.addTransaction(new Transaction(100, "04-12-2024"));
-customer02.addTransaction(new Transaction(200, "04-12-2024"));
-customer01.addTransaction(new Transaction(-50, "05-12-2024"));
-customer02.addTransaction(new Transaction(-50, "05-12-2024"));
-//to get the updated branches
-getAllBranches = bank01.getBranches();
-// console.log(getAllBranches);
-// const getAllCustomers = branch01.customer;
-console.log(branch01.getCustomer());
-// console.log("//----------------------LevelThree---------");
-// console.log(
-//   "The balance for " + customer01.name + "  is " + customer01.getBalance()
-// );
-// console.log(customer01.getBalance()); // Output: 0
-// console.log(customer01.addTransaction(new Transaction(100, "04-12-2024"))); // Deposit 100
-// console.log(customer01.getBalance()); // Output: 100
-// console.log(customer01.addTransaction(new Transaction(-50, "05-12-2024"))); // Withdraw 50
-// console.log(customer01.getBalance()); // Output: 50
-// console.log(customer01.addTransaction(new Transaction(-100, "06-12-2024"))); // Attempt to withdraw 100
-// // Output: "Insufficient balance"
-// console.log(customer01.getBalance()); // Output: 50
-// getAllBranches = bank01.getBranches();
-// console.log("LevelThree: " + getAllBranches);
--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+
+
+//-------------------------Intersection Example--------------
 //delcaring a type 01
 type Person = {
   name: string 
@@ -17,5 +20,40 @@ const student: PersonStudent  = {
   age: 38,
   cohort: "C#"
 }
+
+
+console.log(student)
+
+
+
+
+//-------------------------Union Type Example 01--------------
+
+//two data types assigned to one single variable
+function printMessage(message: string | number){
+  return message;
+}
+
+printMessage("Success");
+printMessage("Failed");
+printMessage("Pending");
+printMessage(200);
+printMessage(404);
+
+
+
+//-------------------------Union Type Example 02--------------
+//a.declaring two data types for the local variable
+type mesg = string | number 
+//b. declaring values of the local variable
+type Status = "Success" | "Failed" | "Pending" | 400 | 200
+
+//c.declaring a function using the the above two types
+function printMessage02(mesg: Status){
+  return mesg;
+}
+
+
+
 
 
