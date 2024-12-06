fc9782c9ee01b99f7fc7fa78caba2719841648bf
Turkiano <turki.saeed.ksa@gmail.com>
2024-12-06 14:30:46 +0300

    Aliases type

--- index.ts
+++ index.ts
@@ -1,17 +1,21 @@
-//delcaring a type 
+//delcaring a type 01
 type Person = {
   name: string 
   age: number
 }
 
+//delcaring a type 02
 type Student = {
-  cohort: "C#"
+  cohort: string
 }
+//Type aliases: can store anything
+type PersonStudent = Person & Student
 
-//assigning the type to an object
-const student: Person  = {
+//assigning the aliases type to an object
+const student: PersonStudent  = {
   name: 'John',
-  age: 38
+  age: 38,
+  cohort: "C#"
 }
 
 
